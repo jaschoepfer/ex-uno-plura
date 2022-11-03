485a3835c1b89f3076f7377916891caf166cf569
JohannSchöpfer <johann.schoepfer@tum.de>
2022-11-03 18:14:23 +0100

    removed all docker related code, renamed sandbox to location

--- exunoplura.py
+++ exunoplura.py
@@ -5,8 +5,6 @@ import string
 from jinja2 import Environment, FileSystemLoader
 #TODO: convert to a package and use PackageLoader
 env = Environment(loader=FileSystemLoader('./templates'))
-import secrets
-import subprocess
 
 @click.group()
 def cli():
@@ -15,19 +13,19 @@ def cli():
 @cli.command()
 @click.option('--server_name', type=click.STRING, default='_')
 def init(server_name):
-    # TODO: check if docker & nginx is installed
+    # TODO: check if nginx is installed
 
     config_dir().mkdir(exist_ok=True)
-    sandbox_conf_dir().mkdir(exist_ok=True)
+    location_conf_dir().mkdir(exist_ok=True)
 
     t = env.get_template('exunoplura.conf')
     central_conf_text = t.render(
             server_name=server_name,
-            sandbox_dir=str(sandbox_conf_dir())
+            location_dir=str(location_conf_dir())
         )
     central_conf().write_text(central_conf_text)
 
-    state = {'sandboxes':[]}
+    state = {'locations':[]}
     state_file().write_text(json.dumps(state))
 
     click.echo(
@@ -54,29 +52,8 @@ def validate_name(ctx, param, name):
 @cli.command()
 @click.option('--http_port', type=click.INT)
 @click.argument('name', type=click.UNPROCESSED, callback=validate_name)
-@click.argument('image', type=click.STRING)
-@click.argument('args', nargs=-1)
 def create(http_port, name, image, args):
-    container_name = f'exunoplura_{name}'
-    password = gen_password() 
-    ssh_port=6001
-
-    try:
-        docker_cmd = [
-                'docker', 'run', '--detach', f'--name={container_name}', '--restart=always',
-                f'--publish={http_port}:80', f'--publish={ssh_port}:2222',
-                '--env=USER_NAME=user', f'--env=USER_PASSWORD={password}',
-                '--env=SUDO_ACCESS=true', '--env=PASSWORD_ACCESS=true',
-                'linuxserver/openssh-server'
-            ]
-        print(' '.join(docker_cmd))
-        subprocess.run(docker_cmd)
-    except Exception as e:
-        click.echo('Could not start container:' + str(e))
-        return
-
-    click.echo(f'Container {container_name} started!')
-    click.echo(f'User password (will not be shown again):{password}')
+    pass
 
 @cli.command()
 def remove():
@@ -84,20 +61,16 @@ def remove():
 
 @cli.command()
 def list():
-    configs = [x for x in sandbox_conf_dir().iterdir() if x.is_file()]
-    click.echo(f'{len(configs)} sandboxes running:')
-
-@cli.command()
-def pwgen():
-    click.echo(gen_password())    
+    configs = [x for x in location_conf_dir().iterdir() if x.is_file()]
+    click.echo(f'{len(configs)} locations set up:')
 
 ## Known Paths
 
 def config_dir():
     return Path.home() / '.exunoplura'
 
-def sandbox_conf_dir():
-    return config_dir() / 'sandbox_nginx_configs'
+def location_conf_dir():
+    return config_dir() / 'location_configs'
 
 def central_conf():
     return config_dir() / 'exunoplura_nginx.conf'
@@ -107,12 +80,5 @@ def state_file():
 
 
 ## Utility Functions
-
-def gen_password():
-    with open('/usr/share/dict/words') as f:
-        words = [word.strip() for word in f if "'" not in word]
-    password = ' '.join(secrets.choice(words) for i in range(4))
-    return password
-
 if __name__ == '__main__':
     cli()
--- test_exunoplura.py
+++ test_exunoplura.py
@@ -18,52 +18,52 @@ def test_first_init(fake_home_dir):
     result = runner.invoke(cli, 'init --server_name www.example.test')
     
     config_dir = fake_home_dir / '.exunoplura'
-    sandbox_conf_dir = config_dir / 'sandbox_nginx_configs'
+    location_conf_dir = config_dir / 'location_configs'
     central_conf = config_dir / 'exunoplura_nginx.conf'
     state_file = config_dir / 'state.json'
 
     assert config_dir.is_dir()
-    assert sandbox_conf_dir.is_dir()
+    assert location_conf_dir.is_dir()
     assert central_conf.is_file()
     assert state_file.is_file()
     central_conf_text = central_conf.read_text()
     assert 'server_name www.example.test;' in central_conf_text
-    assert f'include {sandbox_conf_dir}/*;' in central_conf_text
+    assert f'include {location_conf_dir}/*;' in central_conf_text
     state = json.loads(state_file.read_text())
-    assert state == {'sandboxes':[]}
+    assert state == {'locations':[]}
 
 def test_second_init(fake_home_dir):
     # fill the .exunoplura dir with a mock config
     config_dir = fake_home_dir / '.exunoplura'
-    sandbox_conf_dir = config_dir / 'sandbox_nginx_configs'
+    location_conf_dir = config_dir / 'location_configs'
     central_conf = config_dir / 'exunoplura_nginx.conf'
-    sandbox1_conf = sandbox_conf_dir / 'sandbox1.conf'
-    sandbox2_conf = sandbox_conf_dir / 'sandbox2.conf'
+    location1_conf = location_conf_dir / 'location1.conf'
+    location2_conf = location_conf_dir / 'location2.conf'
     state_file = config_dir / 'state.json'
     
     central_conf_text = 'server { some random; config entries; }'
-    sandbox1_conf_text = 'location { more random; config entries; }'
-    sandbox2_conf_text = 'location { even more; meaningless garble; }'
-    state = {'some':'variable', 'sandboxes':[{'more':'variables'}]}
+    location1_conf_text = 'location { more random; config entries; }'
+    location2_conf_text = 'location { even more; meaningless garble; }'
+    state = {'some':'variable', 'locations':[{'more':'variables'}]}
     
     config_dir.mkdir()
-    sandbox_conf_dir.mkdir()
+    location_conf_dir.mkdir()
     central_conf.write_text(central_conf_text)
-    sandbox1_conf.write_text(sandbox1_conf_text)
-    sandbox2_conf.write_text(sandbox2_conf_text)
+    location1_conf.write_text(location1_conf_text)
+    location2_conf.write_text(location2_conf_text)
     state_file.write_text(json.dumps(state))
 
     test_first_init(fake_home_dir)
     
     # assert that the central config was overwritten
     assert central_conf.read_text() != central_conf_text
-    # assert that sandbox configs were not overwritten
-    assert sandbox1_conf.read_text() == sandbox1_conf_text
-    assert sandbox2_conf.read_text() == sandbox2_conf_text
+    # assert that location configs were not overwritten
+    assert location1_conf.read_text() == location1_conf_text
+    assert location2_conf.read_text() == location2_conf_text
 
 def test_list(fake_home_dir):
-    (fake_home_dir / '.exunoplura' / 'sandbox_nginx_configs').mkdir(parents=True)
+    (fake_home_dir / '.exunoplura' / 'location_configs').mkdir(parents=True)
     runner = CliRunner()
     result = runner.invoke(cli, 'list')
     assert result.exit_code == 0
-    assert '0 sandboxes' in result.output
+    assert '0 locations' in result.output
